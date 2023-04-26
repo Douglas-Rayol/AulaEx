ed86e16aad14c6df88d168dcafc7f40d99fa5043
Douglas <rayolpereira2@gmail.com>
2023-04-26 17:50:11 -0300

    add IF

--- Assets/Script/Condicionais.cs
+++ Assets/Script/Condicionais.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class Condicionais : MonoBehaviour
 {
+    float _f1;
+    float _f2;
+
+    [SerializeField] int _i1;
+    int _i2;
+
+    string _t1;
+    string _t2;
+
+    bool _b1;
+    bool _b2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (_i1 == 2) {
+            Debug.Log("_i1 é igual a 1");
+        }
+        if (_i1 == _i2) {
+            Debug.Log("_i1 é igual a _i2");
+        }
     }
 
     // Update is called once per frame
